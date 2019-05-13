f032884c6bbdd3673110c51992c92314b179961a
Your Namevincentgoot <vincent_first@mail.ru>
2019-05-13 23:10:41 +0500

    Последние правки и слайдер

--- Desktop/Megadom/js/script.js
+++ Desktop/Megadom/js/script.js
@@ -21,3 +21,29 @@ $('#close').on('click', function (){
 $('.search-button').on('click', function(){
     $('.search-input').toggle(100);
 });
+
+$('.bg-video').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    adaptiveHeight: true,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    asNavFor: '.dots',
+    draggable: false,
+    swipe: true
+
+});
+
+$('.dots').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.bg-video',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    pauseOnFocus: false
+  });
